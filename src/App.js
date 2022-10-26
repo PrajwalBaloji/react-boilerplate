f0fa248125e479dde813d9e7dbf16df921de701c
Prajwal Baloji <prajwalbaloji2121996@gmail.com>
2022-10-26 16:12:25 +0530

    added image tag to check accessibility

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ function App() {
       <h1>Hello World !!! {count}</h1>;
       <button onClick={() => setCount(count + 1)}>+</button>
       <button onClick={() => setCount(count > 0 ? count - 1 : 0)}>-</button>
+      <img
+        alt="company logo"
+        src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
+      />
       {count > 10 ? (
         <React.Suspense fallback={null}>
           <Warning />
